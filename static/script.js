06f5452b9ad00d6dace41ebf42d5e541645ab374
lilgmills <laura.a.etale@gmail.com>
2024-12-31 20:00:21 -0600

    refactor using CSS variables

--- static/script.js
+++ static/script.js
@@ -15,131 +15,131 @@ function updateGamepadState() {
             for (let i = 0; i < gamepad.buttons.length; i++) {
                 if (i == 4) {
                     if (gamepad.buttons[i].pressed) {
-                        console.log("button 5 pressed");
-                        document.getElementById('LB').querySelector('svg').querySelector('path').style.fill = 'red'
-                        document.getElementById('LB').querySelector('svg').querySelector('path').style.stroke = 'red'
+                        document.getElementById('LB').querySelector('svg').querySelector('path').classList.remove('inactive')
+                        document.getElementById('LB').querySelector('svg').querySelector('path').classList.add('active')
+                        
                     }
                     else {
-                        document.getElementById('LB').querySelector('svg').querySelector('path').style.fill = 'black'
-                        document.getElementById('LB').querySelector('svg').querySelector('path').style.stroke = 'black'
+                        document.getElementById('LB').querySelector('svg').querySelector('path').classList.remove('active')
+                        document.getElementById('LB').querySelector('svg').querySelector('path').classList.add('inactive')
                     }
                     
                 }
                 else if (i == 5) {
                     if (gamepad.buttons[i].pressed) {
-                        console.log("button 5 pressed");
-                        document.getElementById('RB').querySelector('svg').querySelector('path').style.fill = 'red'
-                        document.getElementById('RB').querySelector('svg').querySelector('path').style.stroke = 'red'
+                        
+                        document.getElementById('RB').querySelector('svg').querySelector('path').classList.remove('inactive')
+                        document.getElementById('RB').querySelector('svg').querySelector('path').classList.add('active')
                     }
                     else {
-                        document.getElementById('RB').querySelector('svg').querySelector('path').style.fill = 'black'
-                        document.getElementById('RB').querySelector('svg').querySelector('path').style.stroke = 'black'
+                        document.getElementById('RB').querySelector('svg').querySelector('path').classList.remove('active')
+                        document.getElementById('RB').querySelector('svg').querySelector('path').classList.add('inactive')
                     }
                     
                 }
                 else if (i==12) {
                     if (gamepad.buttons[i].pressed) {
-                        document.getElementById('up').querySelector('svg').querySelector('path').style.fill = 'red'
-                        document.getElementById('up').querySelector('svg').querySelector('path').style.stroke = 'red'
+                        document.getElementById('up').querySelector('svg').querySelector('path').classList.remove('inactive')
+                        document.getElementById('up').querySelector('svg').querySelector('path').classList.add('active')
                     }
                     else {
-                        document.getElementById('up').querySelector('svg').querySelector('path').style.fill = 'black'
-                        document.getElementById('up').querySelector('svg').querySelector('path').style.stroke = 'black'
+                        document.getElementById('up').querySelector('svg').querySelector('path').classList.remove('active')
+                        document.getElementById('up').querySelector('svg').querySelector('path').classList.add('inactive')
                     }
                     
                 }
                 else if (i==13) {
                     if (gamepad.buttons[i].pressed) {
-                        document.getElementById('down').querySelector('svg').querySelector('path').style.fill = 'red'
-                        document.getElementById('down').querySelector('svg').querySelector('path').style.stroke = 'red'
+                        document.getElementById('down').querySelector('svg').querySelector('path').classList.remove('inactive')
+                        document.getElementById('down').querySelector('svg').querySelector('path').classList.add('active')
                     }
                     else {
-                        document.getElementById('down').querySelector('svg').querySelector('path').style.fill = 'black'
-                        document.getElementById('down').querySelector('svg').querySelector('path').style.stroke = 'black'
+                        document.getElementById('down').querySelector('svg').querySelector('path').classList.remove('active')
+                        document.getElementById('down').querySelector('svg').querySelector('path').classList.add('inactive')
                     }
                     
                 }
                 else if (i==14) {
                     if (gamepad.buttons[i].pressed) {
-                        document.getElementById('left').querySelector('svg').querySelector('path').style.fill = 'red'
-                        document.getElementById('left').querySelector('svg').querySelector('path').style.stroke = 'red'
+                        document.getElementById('left').querySelector('svg').querySelector('path').classList.remove('inactive')
+                        document.getElementById('left').querySelector('svg').querySelector('path').classList.add('active')
                     }
                     else {
-                        document.getElementById('left').querySelector('svg').querySelector('path').style.fill = 'black'
-                        document.getElementById('left').querySelector('svg').querySelector('path').style.stroke = 'black'
+                        document.getElementById('left').querySelector('svg').querySelector('path').classList.remove('active')
+                        document.getElementById('left').querySelector('svg').querySelector('path').classList.add('inactive')
                     }
                     
                 }
                 else if (i==15) {
                     if (gamepad.buttons[i].pressed) {
-                        document.getElementById('right').querySelector('svg').querySelector('path').style.fill = 'red'
-                        document.getElementById('right').querySelector('svg').querySelector('path').style.stroke = 'red'
+                        document.getElementById('right').querySelector('svg').querySelector('path').classList.remove('inactive')
+                        document.getElementById('right').querySelector('svg').querySelector('path').classList.add('active')
                     }
                     else {
-                        document.getElementById('right').querySelector('svg').querySelector('path').style.fill = 'black'
-                        document.getElementById('right').querySelector('svg').querySelector('path').style.stroke = 'black'
+                        document.getElementById('right').querySelector('svg').querySelector('path').classList.remove('active')
+                        document.getElementById('right').querySelector('svg').querySelector('path').classList.add('inactive')
                     }
                     
                 }
                 else if (i == 3) {
                     if (gamepad.buttons[i].pressed) {
-                        document.getElementById('Y').querySelector('svg').querySelector('path').style.fill = 'red'
-                        document.getElementById('Y').querySelector('svg').querySelector('path').style.stroke = 'red'
+                        document.getElementById('Y').querySelector('svg').querySelector('path').classList.remove('inactive')
+                        document.getElementById('Y').querySelector('svg').querySelector('path').classList.add('active')
                     }
                     else {
-                        document.getElementById('Y').querySelector('svg').querySelector('path').style.fill = 'black'
-                        document.getElementById('Y').querySelector('svg').querySelector('path').style.stroke = 'black'
+                        document.getElementById('Y').querySelector('svg').querySelector('path').classList.remove('active')
+                        document.getElementById('Y').querySelector('svg').querySelector('path').classList.add('inactive')
                     }
                 }
                 else if (i == 1) {
                     if (gamepad.buttons[i].pressed) {
-                        document.getElementById('B').querySelector('svg').querySelector('path').style.fill = 'red'
-                        document.getElementById('B').querySelector('svg').querySelector('path').style.stroke = 'red'
+                        document.getElementById('B').querySelector('svg').querySelector('path').classList.remove('inactive')
+                        document.getElementById('B').querySelector('svg').querySelector('path').classList.add('active')
                     }
                     else {
-                        document.getElementById('B').querySelector('svg').querySelector('path').style.fill = 'black'
-                        document.getElementById('B').querySelector('svg').querySelector('path').style.stroke = 'black'
+                        document.getElementById('B').querySelector('svg').querySelector('path').classList.remove('active')
+                        document.getElementById('B').querySelector('svg').querySelector('path').classList.add('inactive')
                     }
                 }
                 else if (i == 0) {
                     if (gamepad.buttons[i].pressed) {
-                        document.getElementById('A').querySelector('svg').querySelector('path').style.fill = 'red'
-                        document.getElementById('A').querySelector('svg').querySelector('path').style.stroke = 'red'
+                        document.getElementById('A').querySelector('svg').querySelector('path').classList.remove('inactive')
+                        document.getElementById('A').querySelector('svg').querySelector('path').classList.add('active')
                     }
                     else {
-                        document.getElementById('A').querySelector('svg').querySelector('path').style.fill = 'black'
-                        document.getElementById('A').querySelector('svg').querySelector('path').style.stroke = 'black'
+                        document.getElementById('A').querySelector('svg').querySelector('path').classList.remove('active')
+                        document.getElementById('A').querySelector('svg').querySelector('path').classList.add('inactive')
                     }
                 }
                 else if (i == 2) {
                     if (gamepad.buttons[i].pressed) {
-                        document.getElementById('X').querySelector('svg').querySelector('path').style.fill = 'red'
-                        document.getElementById('X').querySelector('svg').querySelector('path').style.stroke = 'red'
+                        document.getElementById('X').querySelector('svg').querySelector('path').classList.remove('inactive')
+                        document.getElementById('X').querySelector('svg').querySelector('path').classList.add('active')
                     }
                     else {
-                        document.getElementById('X').querySelector('svg').querySelector('path').style.fill = 'black'
-                        document.getElementById('X').querySelector('svg').querySelector('path').style.stroke = 'black'
+                        document.getElementById('X').querySelector('svg').querySelector('path').classList.remove('active')
+                        document.getElementById('X').querySelector('svg').querySelector('path').classList.add('inactive')
                     }
                 }
                 else if (i == 8) {
                     if (gamepad.buttons[i].pressed) {
-                        document.getElementById('view').querySelector('svg').querySelector('path').style.fill = 'red'
-                        document.getElementById('view').querySelector('svg').querySelector('path').style.stroke = 'red'
+                        document.getElementById('view').querySelector('svg').querySelector('path').classList.remove('inactive')
+                        document.getElementById('view').querySelector('svg').querySelector('path').classList.add('active')
                     }
                     else {
-                        document.getElementById('view').querySelector('svg').querySelector('path').style.fill = 'black'
-                        document.getElementById('view').querySelector('svg').querySelector('path').style.stroke = 'black'
+                        document.getElementById('view').querySelector('svg').querySelector('path').classList.remove('active')
+                        document.getElementById('view').querySelector('svg').querySelector('path').classList.add('inactive')
                     }
 
                 }
                 else if (i == 9) {
                     if (gamepad.buttons[i].pressed) {
-                        document.getElementById('menu').querySelector('svg').querySelector('path').style.fill = 'red'
-                        document.getElementById('menu').querySelector('svg').querySelector('path').style.stroke = 'red'
+                        document.getElementById('menu').querySelector('svg').querySelector('path').classList.remove('inactive')
+                        document.getElementById('menu').querySelector('svg').querySelector('path').classList.add('active')
                     }
                     else {
-                        document.getElementById('menu').querySelector('svg').querySelector('path').style.fill = 'black'
-                        document.getElementById('menu').querySelector('svg').querySelector('path').style.stroke = 'black'
+                        document.getElementById('menu').querySelector('svg').querySelector('path').classList.remove('active')
+                        document.getElementById('menu').querySelector('svg').querySelector('path').classList.add('inactive')
                     }
 
                 }
@@ -159,20 +159,20 @@ function updateGamepadState() {
                 rAxis.style.transform = `translate(${6*gamepad.axes[2] +"px"}, ${6*gamepad.axes[3]+"px"} ) rotate3d(0,1,0,${30*gamepad.axes[2]}deg) rotate3d(-1,0,0,${30*gamepad.axes[3]}deg)`;
 
                 if (gamepad.axes[0]**2 + gamepad.axes[1]**2 > .09) {
-                    lAxis.querySelector('svg').querySelector('path').style.fill = 'red'
-                    lAxis.querySelector('svg').querySelector('path').style.stroke = 'red'
+                    lAxis.querySelector('svg').querySelector('path').classList.remove('inactive')
+                    lAxis.querySelector('svg').querySelector('path').classList.add('active')
                 }
                 else {
-                    lAxis.querySelector('svg').querySelector('path').style.fill = 'black'
-                    lAxis.querySelector('svg').querySelector('path').style.stroke = 'black'
+                    lAxis.querySelector('svg').querySelector('path').classList.remove('active')
+                    lAxis.querySelector('svg').querySelector('path').classList.add('inactive')
                 }
                 if (gamepad.axes[2]**2 + gamepad.axes[3]**2 > .09) {
-                    rAxis.querySelector('svg').querySelector('path').style.fill = 'red'
-                    rAxis.querySelector('svg').querySelector('path').style.stroke = 'red'
+                    rAxis.querySelector('svg').querySelector('path').classList.remove('inactive')
+                    rAxis.querySelector('svg').querySelector('path').classList.add('active')
                 }
                 else {
-                    rAxis.querySelector('svg').querySelector('path').style.fill = 'black'
-                    rAxis.querySelector('svg').querySelector('path').style.stroke = 'black'
+                    rAxis.querySelector('svg').querySelector('path').classList.remove('active')
+                    rAxis.querySelector('svg').querySelector('path').classList.add('inactive')
                 }
                 
                 
